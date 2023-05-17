f027af38a93a12b738ccfacc6818ed2959d3021e
ViniS07 <92175803+ViniciusF07@users.noreply.github.com>
2023-05-17 14:48:37 -0300

    Update settings.py

--- tarefas/tarefas/settings.py
+++ tarefas/tarefas/settings.py
@@ -56,7 +56,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'tarefas.urls'
 
